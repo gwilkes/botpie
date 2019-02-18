1364ce30ff8005c2bc99efaaf69ca5c74ba80de7
Grant Wilkes <grant.wilkes@gmail.com>
2019-02-18 01:44:08 -0500

    updated to version 0.0.2

--- botpie/__init__.py
+++ botpie/__init__.py
@@ -2,7 +2,7 @@ __title__ = "botpie"
 __author__ = "gwilkes"
 __license__ = "MIT"
 __copyright__ = "Copyright 2019 gwilkes"
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 
 from .cmd import Command
 from .bot import Bot
